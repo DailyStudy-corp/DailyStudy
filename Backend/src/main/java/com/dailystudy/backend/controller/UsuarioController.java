b512c2ff5f4e0d1f5ad6f9a3797863acaa6bdce4
PE-zc <pauloernesto2004@gmail.com>
2026-06-08 10:46:57 -0300

    fix: Retornando token como JSON

--- Backend/src/main/java/com/dailystudy/backend/controller/UsuarioController.java
+++ Backend/src/main/java/com/dailystudy/backend/controller/UsuarioController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/usuarios")
@@ -27,10 +29,10 @@ public class UsuarioController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login (@Valid @RequestBody LoginDTO dto) {
+    public ResponseEntity<Map<String, String>> login (@Valid @RequestBody LoginDTO dto) {
         String token = usuarioService.autenticar(dto);
 
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(Map.of("token", token));
     }
 
 }
