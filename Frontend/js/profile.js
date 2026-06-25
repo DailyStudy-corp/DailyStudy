3c32bce35b4511afee1ebbf743688661b3c0a675
gyo7b <guilhermeca775@gmail.com>
2026-06-25 08:35:13 -0300

    Correcao do caminho do Profile

--- Frontend/js/profile.js
+++ Frontend/js/profile.js
@@ -207,8 +207,8 @@ const Profile = (() => {
         img_perfil: type === 'avatar' ? dataUrl : currentProfile.img_perfil,
         banner_perfil: type === 'banner' ? dataUrl : currentProfile.banner_perfil
       };
-                                      ///api/usuarios/me/img_perfil
-      const response = await fetch('/api/usuarios/me/img_perfil', {
+                                      //localhost:8080/api/usuarios/me
+      const response = await fetch('http://localhost:8080/api/usuarios/me/img_perfil', {
         method: 'PUT',
         headers: {
           'Authorization': `Bearer ${token}`,
