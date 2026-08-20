e3ff6c53bbbbd248fad457aa37e3c623f65142f5
PE-zc <pauloernesto2004@gmail.com>
2026-08-20 20:55:17 -0300

    fix: Anotacoes que validam o DTO do comentario

--- Backend/src/main/java/com/dailystudy/backend/dto/ComentarioDTO.java
+++ Backend/src/main/java/com/dailystudy/backend/dto/ComentarioDTO.java
@@ -1,4 +1,10 @@
 package com.dailystudy.backend.dto;
 
-public record ComentarioDTO(String content, String mediaUrl) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ComentarioDTO(@NotBlank(message = "O conteúdo não pode estar vazio")
+                            @Size(max = 500, message = "O conteudo deve ter no máximo 500 caracteres")
+                            String content,
+                            String mediaUrl) {
 }
