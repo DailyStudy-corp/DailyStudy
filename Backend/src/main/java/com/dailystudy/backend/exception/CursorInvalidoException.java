f49616afa3cc433dd625b1a6730c4a81f5b0f714
PE-zc <pauloernesto2004@gmail.com>
2026-08-26 20:28:22 -0300

    Arquivos de captura de excecao e solucao para datas da paginacao

---
+++ Backend/src/main/java/com/dailystudy/backend/exception/CursorInvalidoException.java
@@ -0,0 +1,5 @@
+package com.dailystudy.backend.exception;
+
+public class CursorInvalidoException extends RuntimeException {
+    public CursorInvalidoException (String message) {super(message);}
+}
